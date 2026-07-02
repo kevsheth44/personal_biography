895d958810ebaaf8ab56aac8c663ad8e83a72c9e
Keval <shethkev@gmail.com>
2026-07-02 14:59:20 -0500

    Added flip card bounce, hero dynamic background, side project carosel, etc

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,5 +4,11 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert_select "img[style*='--image-fit']", minimum: 1
+    assert_select ".project-carousel", minimum: 1
+    assert_select ".project-card-link", minimum: 1
+    assert_select ".project-carousel-dots", minimum: 1
+    assert_select "a.nav-link[href='#about']", minimum: 1
+    assert_select "section[id='about']", minimum: 1
   end
 end
